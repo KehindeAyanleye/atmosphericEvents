e39ae8601dcefdad194376ce06495cb4a802c34b
Kehinde <kehindeayanleye@gmail.com>
2015-10-09 09:36:58 +0100

    Rspec test

--- spec/requests/atmospheric_pages_spec.rb
+++ spec/requests/atmospheric_pages_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "AtmosphericPages", type: :request do
+
   describe "GET /atmospheric_pages" do
     it "works! (now write some real specs)" do
-      get atmospheric_pages_index_path
+      get atmospheric_pages_home_path
       expect(response).to have_http_status(200)
     end
   end
+
+	describe "GET /atmospheric_pages" do
+		it "works! (now write some real specs)" do
+		  get atmospheric_pages_about_path
+		  expect(response).to have_http_status(200)
+		end
+    end
+
 end
