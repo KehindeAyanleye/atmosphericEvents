8a7a1a74cd6bfeab721750b0a9230cf86b6d0867
Kehinde <kehindeayanleye@gmail.com>
2015-10-24 11:49:05 +0100

    js

---
+++ app/assets/javascripts/qoute.js
@@ -0,0 +1,26 @@
+$(function() {
+  var slideCounter = 1;
+  
+  $(".next-button").click(function() {
+    var currentSlide = $("#slide" + slideCounter);
+    var nextSlide = $("#slide" + (slideCounter + 1));
+    var prevSlide = $("#slide" + (slideCounter - 1));
+    currentSlide.removeClass("current");
+    currentSlide.addClass("hidden");
+    nextSlide.removeClass("hidden");
+    nextSlide.addClass("current");
+    slideCounter += 1;
+  });
+  
+  $(".prev-button").click(function() {
+    var currentSlide = $("#slide" + slideCounter);
+    var nextSlide = $("#slide" + (slideCounter + 1));
+    var prevSlide = $("#slide" + (slideCounter - 1));
+    
+    currentSlide.removeClass("current");
+    currentSlide.addClass("hidden");
+    prevSlide.addClass("current");
+    slideCounter -= 1;
+  });
+  
+});
