69150b68a8dd5a76202c2f37b7fe2ee0da5d927c
Taiwo <mrtaiwoayanleye@gmail.com>
2017-03-19 00:11:49 +0100

    rename .css.scss to .scss

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,22 +2,8 @@ class PagesController < ApplicationController
   def home
   end
 
-  def gallery
-  end
-
   def contact
         @quote = Quote.new
-
-  #   @quote = Quote.new(params[:quote])
-  #   @quote.request = request
-  #   if @quote.deliver
-  #     # flash.now[:error] = nil
-  #     flash[:notice] = "Thank you for your quote!"
-  #     redirect_to root_path
-  #   else
-  #     flash[:error] = "Cannot send quote."
-  #     render :new
-  #   end
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :photos
   mount RailsAdmin::Engine => '/moana', as: 'rails_admin'
   get 'pages/home'
-  get 'pages/gallery'
   get 'pages/contact'
   get 'pages/about'
   get 'pages/service'
